0b6ed9166b94af99de5e86afe46edeb9d7ec0a5d
Jeffrey Monja Castro <jeffrey.monja@utec.edu.pe>
2025-04-24 14:17:33 -0500

    CF

---
+++ CP/CF/1020D3/C.cpp
@@ -0,0 +1,96 @@
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+#define cpu()                                                                  \
+  ios::sync_with_stdio(false);                                                 \
+  cin.tie(nullptr);
+#define ll long long
+#define lld long double
+const int mod = 1e9 + 7;
+
+void solve() {
+  int n, k, x = -1;
+  cin >> n >> k;
+  vector<int> a(n);
+  vector<int> b(n);
+
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+
+  for (int i = 0; i < n; i++) {
+    cin >> b[i];
+  }
+
+  for (int i = 0; i < n; i++) {
+    if (b[i] == -1)
+      continue;
+
+    int curr = a[i] + b[i];
+
+    if (curr > k) {
+      cout << "0" << endl;
+      return;
+    }
+
+    if (x == -1) {
+      x = curr;
+    } else if (x != curr) {
+      cout << "0" << endl;
+      return;
+    }
+  }
+
+  if (x != -1) {
+    for (int i = 0; i < n; i++) {
+      if (b[i] != -1)
+        continue;
+      int val = x - a[i];
+      if (val < 0 || val > k) {
+        cout << "0" << endl;
+        return;
+      }
+    }
+    cout << "1" << endl;
+  } else {
+    set<int> pos;
+
+    for (int i = 0; i <= 2 * k; i++) {
+      bool flag = true;
+
+      for (int j = 0; j < n; j++) {
+        int val = i - a[j];
+
+        if (val < 0 || val > k) {
+          flag = false;
+          break;
+        }
+      }
+
+      if (flag) {
+        pos.insert(i);
+      }
+    }
+
+    cout << pos.size() << endl;
+
+    // int max_x = *(max_element(a.begin(), a.end()));
+    // int min_x = *(min_element(a.begin(), a.end()));
+
+    // cout << (k + 1) - (max_x - min_x) << endl;
+  }
+}
+
+int main() {
+  cpu();
+  int t;
+  t = 1;
+  cin >> t;
+  while (t--)
+    solve();
+  return 0;
+}
